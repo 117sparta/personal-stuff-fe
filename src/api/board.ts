27a6dd8c07d4aa6667d902588ac0ad7aa4fc1204
117sparta <1246286991@qq.com>
2020-03-15 23:58:07 +0800

    feat: api config

--- src/api/board.ts
+++ src/api/board.ts
@@ -1,1 +1,12 @@
-export default 'board';
+import requestObj from '@/api/config/request.config';
+
+function createBoard (board) {
+  return requestObj.post(
+    '/board/createBoard',
+    board
+  );
+}
+
+export default {
+  createBoard
+};
---
+++ src/api/config/request.config.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  timeout: 5000,
+  withCredentials: true,
+  baseURL: 'http://localhost:3000'
+});
+
+instance.interceptors.request.use(config => {
+  if (config.method.toLocaleLowerCase() === 'post') {
+    config.headers['Content-type'] = 'application/x-www-form-urlencoded';
+  }
+  return config;
+}, error => {
+  return Promise.reject(error);
+});
+
+instance.interceptors.response.use(response => {
+  return Promise.resolve(response);
+}, error => {
+  return Promise.reject(error);
+});
+
+export default instance;
